76e3b9f76ee9456f06fc4b5d6c5711915f45b2ab
kcmsnn <kcmsn.ccna@gmail.com>
2021-03-27 19:54:15 +0800

    Sakila III updated

--- SQL queries/Sakila III.sql
+++ SQL queries/Sakila III.sql
@@ -6,7 +6,7 @@ INNER JOIN country ON city.country_id = country.country_id
 GROUP BY country;
 
 #2. How many customers are there for each city?  Have your result display the full city name, the full country name, as well as the number of customers for each city.
-SELECT country, city, COUNT(address.address_id) AS total_number_of_customer FROM customer
+SELECT country, city, COUNT(customer_id) AS total_number_of_customer FROM customer
 RIGHT JOIN address ON customer.address_id = address.address_id
 INNER JOIN city ON address.city_id = city.city_id
 LEFT JOIN country ON city.country_id = country.country_id
@@ -16,6 +16,15 @@ ORDER BY country;
 #Now, look at the payment table where it has information about the customer as well as how much the customer paid to rent the item.  Based on this,
 
 #1. Retrieve both the average rental amount, the total rental amount, as well as the total number of transactions for each month of each year.
+SELECT date_format(payment_date, '%M - %Y') AS Month_and_Year, 
+SUM(amount) AS total_rental_amount, 
+COUNT(payment_id) AS total_transactions, 
+AVG(amount) AS Average_rental_amount FROM payment
+GROUP BY date_format(payment_date, '%M - %Y');
 
 #2. Your manager comes and asks you to pull payment report based on the hour of the day.  The managers wants to know which hour the company earns the most money/payment.
 #	Have your sql query generate the top 10 hours of the day with the most sales.  Have the first row of your result be the hour with the most payments received.
+SELECT DATE_FORMAT(payment_date, '%h %p') AS Month_and_Year, SUM(amount) AS total_payments_received FROM payment
+GROUP BY DATE_FORMAT(payment_date, '%h %p')
+ORDER BY total_payments_received DESC
+LIMIT 10;
