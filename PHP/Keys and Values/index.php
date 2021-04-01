bd7f485cde28149395bb9279b3bce2b5d82e12ff
kcmsnn <kcmsn.ccna@gmail.com>
2021-04-01 15:48:20 +0800

    Keys and Values Added

---
+++ PHP/Keys and Values/index.php
@@ -0,0 +1,20 @@
+<?php
+
+
+function keys_values(){
+
+    $users['first_name'] = "Michael";
+    $users['last_name'] = "Choi";
+
+    echo "There are " . count($users) . " keys in this array:<br>";    
+    foreach ($users as $key => $value) {
+        echo $key."<br/>";
+    }
+    foreach ($users as $key => $value) {
+        echo "The value in the key '". $key . "' is '" . $value . "'.<br>";
+    }
+
+}
+$A = keys_values();
+echo $A;
+?>
